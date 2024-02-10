fe0579a3deed367b44c7fd28531b72ebf83bba2d
allenleejy <leejunyeeallen@gmail.com>
2024-02-10 17:57:44 +0800

    Pairwork

---
+++ app/src/main/java/com/example/csproject/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.csproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+    private lateinit var drawerLayout:DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        getSupportActionBar()?.setDisplayShowTitleEnabled(false)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_cart -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,CartFragment()).commit()
+        }
+        when(item.itemId){
+            R.id.nav_member -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,MemberFragment()).commit()
+        }
+        when(item.itemId){
+            R.id.nav_rate -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,RatingFragment()).commit()
+        }
+        when(item.itemId){
+            R.id.nav_home -> {val intent = Intent(applicationContext, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    startActivity(intent)}
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed(){
+        super.onBackPressed()
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
